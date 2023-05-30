2b55ff5ef8c7b3cbd35e6c26c9c0e1c8428afcd4
ManinEgor <115859812+ManinEgor@users.noreply.github.com>
2023-05-30 18:57:10 +0500

    Create tests.py

---
+++ .github/workflows/tests.py
@@ -0,0 +1,18 @@
+import unittest
+import requests
+
+def get_status(url):
+    try:
+        status = requests.get(url)
+        return status.status_code
+    except:
+        return 404
+
+
+class TestCase(unittest.TestCase):
+    def testCode(self):
+        self.assertEqual(200, get_status('https://www.sdfsdfffd.com/'))
+
+if __name__ == '__main__':
+    tests = TestCase()
+    print(tests.testCode())
